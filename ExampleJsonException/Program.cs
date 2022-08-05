f111c51f3f357335504946e38d4521046c104f03
new_user <new_user@gmail.com>
2022-08-05 18:56:56 +0300

    Добавьте файлы проекта.

---
+++ ExampleJsonException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+string json = File.ReadAllText("load.txt");
+
+var result = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<TagsPhoto>>(json);//First element null
+var resultSecond = JsonConvert.DeserializeObject<IEnumerable<TagsPhoto>>(json);
+Console.WriteLine(result.First().Tag ?? "null");// Tag == null
+Console.WriteLine(resultSecond.First().Tag);//Tag is not null
+
+Console.ReadKey();
+
+
+public class TagsPhoto
+{
+    public string Tag { set; get; }
+    public float Probability { set; get; }
+}
+
